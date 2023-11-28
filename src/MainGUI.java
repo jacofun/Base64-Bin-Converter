33bb55e54423ac378addce7aa2a97d8aae3f226c
JACO <97010826+jacofun@users.noreply.github.com>
2023-11-28 20:33:51 +0800

    Update MainGUI.java

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -3,60 +3,59 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 
-public class MainGUI extends JFrame {
+public class ConverterGUI extends JFrame {
 
-    private final JFileChooser fileChooser;
+    private JButton zipToBase64Button;
+    private JButton base64ToZipButton;
+    private JFileChooser fileChooser;
 
-
-    public MainGUI() {
-        this.setTitle("File Converter");
-        this.setSize(600, 200);
+    public ConverterGUI() {
+        this.setTitle("Base64/Bin Converter");
+        this.setSize(400, 200);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container pane = this.getContentPane();
-        pane.setLayout(new GridLayout(1, 2));
+        pane.setLayout(new GridLayout(3, 1));
 
-        fileChooser = new JFileChooser();
 
-        JButton zipToBase64Button = new JButton("ZIP to Base64");
-
+        zipToBase64Button = new JButton("Convert Bin to Base64");
         zipToBase64Button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                fileChooser.setMultiSelectionEnabled(false);
+                fileChooser = new JFileChooser();
                 int returnValue = fileChooser.showOpenDialog(null);
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
                     File selectedFile = fileChooser.getSelectedFile();
-                    try {
-                        Converter.toBase64(selectedFile, 6);
-                        JOptionPane.showMessageDialog(null, "ZIP to Base64 Done!", "SUCCESS", JOptionPane.INFORMATION_MESSAGE);
-                    } catch (IOException error) {
-                        JOptionPane.showMessageDialog(null, error.getMessage(), "FAIL", JOptionPane.INFORMATION_MESSAGE);
-                    }
+					String inputFile = selectedFile.getAbsolutePath();
 
-
+					Converter.toBase64(inputFile);
+					JOptionPane.showMessageDialog(null, "Done", "Notice", JOptionPane.INFORMATION_MESSAGE);
+                    // call your ZiptoBase64 method here
+                    // ZiptoBase64.convert(selectedFile);
                 }
 				
             }
         });
 
-        JButton base64ToZipButton = new JButton("Base64 to ZIP");
+        base64ToZipButton = new JButton("Convert Base64 to Bin");
         base64ToZipButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
-                fileChooser.setMultiSelectionEnabled(true);
+                fileChooser = new JFileChooser();
+				// enable mutiSelect
+				fileChooser.setMultiSelectionEnabled(true);
                 int returnValue = fileChooser.showOpenDialog(null);
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
                     File[] selectedFiles = fileChooser.getSelectedFiles();
-                    String outputFileName = "TestZip.zip";
-                    try {
-                        Converter.toZip(outputFileName, selectedFiles);
-                        JOptionPane.showMessageDialog(null, "Base64 to ZIP done", "Notice", JOptionPane.INFORMATION_MESSAGE);
-                    } catch (IOException error) {
-                        JOptionPane.showMessageDialog(null, error.getMessage(), "FAIL", JOptionPane.INFORMATION_MESSAGE);
-                    }
+					String[] inputFiles = new String[selectedFiles.length];
+					for(int i = 0; i < selectedFiles.length; i++) {
+						inputFiles[i] = selectedFiles[i].getAbsolutePath();
+					}
 
+
+                    JOptionPane.showMessageDialog(null, Converter.toBin(inputFiles), "Notice", JOptionPane.INFORMATION_MESSAGE);
+					//JOptionPane.showMessageDialog(null, inputFiles, "Notice", JOptionPane.INFORMATION_MESSAGE);					
+                    // call your CombineTozip method here
+                    // CombineTozip.convert(selectedFile);
                 }
             }
         });
@@ -66,7 +65,7 @@ public class MainGUI extends JFrame {
     }
 
     public static void main(String[] args) {
-        MainGUI app = new MainGUI();
+        ConverterGUI app = new ConverterGUI();
         app.setVisible(true);
     }
 }
